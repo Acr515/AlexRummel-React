28565ae332e6363623bc1b2f0ffaa7d00784042c
Acr515 <rummelalexander@outlook.com>
2025-04-17 11:37:52 -0400

    Created copy of Craigslist to serve as Launchpad base

---
+++ src/projects/Launchpad/imports.js
@@ -0,0 +1,27 @@
+/**
+ * A dictionary of file names in the assets folder, minus the file extension. Used to avoid polluting the screen .jsx file for a portfolio entry.
+ */
+const imports = {
+    "user-flow-a": require('./assets/user-flow-a.png'),
+    "user-flow-b": require('./assets/user-flow-b.png'),
+    "sketch-1": require('./assets/sketch-1.jpg'),
+    "sketch-2": require('./assets/sketch-2.jpg'),
+    "sketch-3": require('./assets/sketch-3.jpg'),
+    "sketch-4": require('./assets/sketch-4.jpg'),
+    "sketch-5": require('./assets/sketch-5.jpg'),
+    "sketch-6": require('./assets/sketch-6.jpg'),
+    "wireframe": require('./assets/wireframe.png'),
+    "style-guide": require('./assets/style-guide.png'),
+    "button-animations": require('./assets/button-animations.gif'),
+    "create-details": require('./assets/prototype/create-details.png'),
+    "create-draft": require('./assets/prototype/create-draft.png'),
+    "create-images": require('./assets/prototype/create-images.png'),
+    "create-landing": require('./assets/prototype/create-landing.png'),
+    "home-page-large": require('./assets/prototype/home-page-large.png'),
+    "listing-details": require('./assets/prototype/listing-details.png'),
+    "listing-gallery": require('./assets/prototype/listing-gallery.png'),
+    "small-cover": require('./small-cover.jpg'),
+    "listing-map": require('./assets/prototype/listing-map.png')
+};
+
+export default imports;
